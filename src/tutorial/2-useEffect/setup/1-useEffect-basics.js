786925765a64c8588bf8bb08a03460605043cad9
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-29 22:53:01 +0530

    useEffect - Dependency List

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,14 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
   useEffect(() => {
+      console.log('useEffect called');
     // useEffect or hooks cannot be placed inside conditional statements
     // but consitional statement can be used inside useEffect callback function
     if(value > 0){
       document.querySelector('title').innerText = `New message(${value})`;
     }  
-  });
-  const [value, setValue] = useState(0);
+  }, [value]);
+  // second parameter is array of dependencies
+  // whenever the dependency value gets updated useEffect is run
+  // if second parameter of useEffect is [] empty array, it says run on initial render only
+  
+  useEffect(() => {
+    console.log('hello world');
+  }, []);
+
   return (
     <>
       <h1>{value}</h1>
